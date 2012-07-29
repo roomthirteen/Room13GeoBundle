aa6d5c5ccbc0b73987147e8e002d0be205c7a027
Benjamin Seibert <benjamin.seibert@gmx.de>
2012-07-29 02:45:08 +0200

    wrapped script in jquery boilerplate

--- Resources/public/jquery.ui.location-autocomplete.js
+++ Resources/public/jquery.ui.location-autocomplete.js
@@ -1,70 +1,117 @@
+// the semi-colon before function invocation is a safety net against concatenated
+// scripts and/or other plugins which may not be closed properly.
+;(function ( $, window, undefined ) {
 
-$.fn.room13GeoLocationWidget = function(){
+    // Create the defaults once
+    var pluginName = 'room13GeoLocationWidget',
+        document = window.document,
+        defaults = {
+            propertyName: "value"
+        };
 
+    // The actual plugin constructor
+    function Plugin( element, options ) {
+        this.element = element;
 
+        // jQuery has an extend method which merges the contents of two or
+        // more objects, storing the result in the first object. The first object
+        // is generally empty as we don't want to alter the default options for
+        // future instances of the plugin
+        this.options = $.extend( {}, defaults, options) ;
 
-    var $this     = $(this),
-        sourceUrl = $this.data('source'),
-        input     = $this.find('input[type=text]'),
-        output    = $this.find('input[type=hidden]'),
-        loader    = $this.find('.Loader');
+        this._defaults = defaults;
+        this._name = pluginName;
 
-
-    function inputFocused()
-    {
-        this.select();
+        this.init();
     }
 
-    function dataLoaded(data,response)
-    {
-        // publish autocomplete data
-        response(data);
+    Plugin.prototype.init = function () {
 
-        // hide the loader
-        loader.hide();
 
-        // clean value if no results where found
-        if(data.length==0)
+        var target     = $(this.element),
+            input     = target.find('input[type=text]'),
+            output    = target.find('input[type=hidden]'),
+            loader    = target.find('.Loader');
+
+
+        function inputFocused()
         {
-            output.val('');
+            target.select();
         }
+
+        function dataLoaded(data,response)
+        {
+            // publish autocomplete data
+            response(data);
+
+            // hide the loader
+            loader.hide();
+
+            // clean value if no results where found
+            if(data.length==0)
+            {
+                output.val('');
+            }
+        }
+
+        function locationSelected(evt,ui)
+        {
+            // write back id to hidden field
+            // the input field will be populated automaticly by the autocomplete widget
+            output.val(ui.item.id);
+        }
+
+        function searchStarted()
+        {
+            // show the load indicator
+            loader.show();
+        }
+
+        input.focus(inputFocused);
+
+        input.autocomplete({
+            minLength: 1,
+            source: function (request, response) {
+
+                sourceUrl = target.data('source');
+                console.log(sourceUrl);
+
+                $.ajax({
+                    url: sourceUrl,
+                    dataType: "json",
+                    data: request,
+                    success: function(data){
+                        dataLoaded(data,response)
+                    }
+                });
+            },
+
+            select: locationSelected,
+            search: searchStarted
+        });
+
     }
 
-    function locationSelected(evt,ui)
-    {
-        // write back id to hidden field
-        // the input field will be populated automaticly by the autocomplete widget
-        output.val(ui.item.id);
+
+
+
+
+    // A really lightweight plugin wrapper around the constructor,
+    // preventing against multiple instantiations
+    $.fn[pluginName] = function ( options ) {
+        return this.each(function () {
+            if (!$.data(this, 'plugin_' + pluginName)) {
+                $.data(this, 'plugin_' + pluginName, new Plugin( this, options ));
+            }
+        });
     }
 
-    function searchStarted()
-    {
-        // show the load indicator
-        loader.show();
-    }
-
-    input.focus(inputFocused);
-
-    input.autocomplete({
-        minLength: 1,
-        source: function (request, response) {
-            $.ajax({
-                url: sourceUrl,
-                dataType: "json",
-                data: request,
-                success: function(data){
-                    dataLoaded(data,response)
-                }
-            });
-        },
-
-        select: locationSelected,
-        search: searchStarted
-    });
-
-}
+}(jQuery, window));
 
 
-$('.Room13GeoLocationWidget').each(function(){
-    $(this).room13GeoLocationWidget();
+
+
+
+$(function(){
+    $('.Room13GeoLocationWidget').room13GeoLocationWidget();
 });
